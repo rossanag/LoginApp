f109e609ad3f6434cfa8293ab8047b9fab319a23
rossanag <guerra.rossana@gmail.com>
2023-03-02 20:27:16 -0300

    Change structure folder

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,7 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 import eslint from 'vite-plugin-eslint';
+import path from 'path';
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -8,4 +9,9 @@ export default defineConfig({
 	server: {
 		open: '/index.html',
 	}, 
+	resolve: {
+		alias: {
+			'@root': path.resolve(__dirname, ''),
+		},
+	},
 });
